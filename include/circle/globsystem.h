d8c03db080d43dd49de385f1ce396a91873169ac
Blackaddr <slascos@gmail.com>
2023-06-01 07:25:23 -0400

    Add globsystem to library to support global access to devices

---
+++ include/circle/globsystem.h
@@ -0,0 +1,23 @@
+#ifndef _globsystem_h
+#define _globsystem_h
+
+#include <circle/types.h>
+#include <circle/logger.h>
+#include <circle/screen.h>
+
+
+// Logger
+#define LogScreenMsg(Msg, MsgLength) do { if (g_ScreenPtr) g_ScreenPtr->Write(Msg, MsgLength); } while(0)
+#define LogScreen(Msg) do { if (g_ScreenPtr) { CString msg; msg.Format(Msg); g_ScreenPtr->Write(msg, msg.GetLength());} } while(0)
+#define LogSerial(Source, Severity, Msg) do { if (g_LoggerPtr) g_LoggerPtr->Write(Source, Severity, Msg); } while(0)
+
+extern CLogger*       g_LoggerPtr;
+extern CScreenDevice* g_ScreenPtr;
+
+// Interrupt System
+extern CInterruptSystem* g_interruptSysPtr;
+
+// Timer
+#define delay(x) do { CTimer::Get()->MsDelay(x);} while(0)
+
+#endif
---
+++ lib/globsystem.cpp
@@ -0,0 +1,8 @@
+#include <circle/globsystem.h>
+
+// Logging
+CLogger*       g_LoggerPtr = nullptr;
+CScreenDevice* g_ScreenPtr = nullptr;
+
+// Peripherals
+CInterruptSystem* g_interruptSysPtr = nullptr;
